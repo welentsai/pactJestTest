f8dcb3ed6b8bfa097737748f764a774c606b31da
Tsai WeiLun <welentsai@Tsaidebijixingdiannao.local>
2023-04-18 22:56:50 +0800

    add vitest

--- test/client.spec.ts
+++ test/client.spec.ts
@@ -1,14 +1,14 @@
 // test/clinet.spec.ts
-import getUser from '../src/client';
+import {api} from '../src/client';
+import { assert, expect, test } from 'vitest'
 
 test('getUser', async () => {
   // Arrange
+  const client = api()
   
 
   // Act
-  await getUser()
+  await client.getUsers()
   .then( (value) => console.log('get Value=>', value))
   .catch((reason) => console.log('failed reason is =>', reason))
-
-
 });
